7249c844c0f5a013919187c9517f4f54fcbd359a
pranali <sahilprakashthorat@gmail.com>
2026-01-14 00:14:15 +0530

    new changes

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,9 +1,10 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import generateToken from './../utils/token';
 
-const signUp = async(req, res) => {
+export const signUp = async(req, res) => {
     try {
-        const { fullName, email, password, mobileNumber, role } = req.body;
+        const { fullName, email, role, mobileNumber, password } = req.body;
         const user = await User.findOne({ email });
         if (user) {
             return res.status(400).json({ message: "User already exists" });
@@ -21,10 +22,66 @@ const signUp = async(req, res) => {
             fullName,
             email,
             role,
+            mobileNumber,
             password: hashedPassword,
-            mobileNumber
         });
-        res.status(201).json({ message: "User created successfully", user });
+
+        const token = await generateToken(user._id);
+        res.cookie("token", token, {
+            secure: false,
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 30 days
+            httpOnly: true,
+        }
+        )
+
+        res.status(201).json({
+            message: "User created successfully", user })  
+
+    } catch (error) {
+        console.error('Error during sign up:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+  
+
+};
+
+export const signIn = async(req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
+        if (mobileNumber.length < 6) {
+            return res.status(400).json({ message: "Mobile number must be at least 10 digits long" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        user = await User.create({
+            fullName,
+            email,
+            role,
+            mobileNumber,
+            password: hashedPassword,
+        });
+
+        const token = await generateToken(user._id);
+        res.cookie("token", token, {
+            secure: false,
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 30 days
+            httpOnly: true,
+        }
+        )
+
+        res.status(201).json({
+            message: "User created successfully", user })  
+
     } catch (error) {
         console.error('Error during sign up:', error);
         res.status(500).json({ message: 'Internal server error' });
--- backend/utils/token.js
+++ backend/utils/token.js
@@ -1,3 +1,10 @@
-const generateToken = async(userId) => {
+import jwt from 'jsonwebtoken';
 
+const generateToken = async(userId) => {
+    const token = await jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+    return token;
 }
+
+export default generateToken;
