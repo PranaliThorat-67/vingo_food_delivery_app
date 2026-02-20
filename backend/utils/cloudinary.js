92dba762574184e41982d75fea5e2c99e1968c75
pranali <sahilprakashthorat@gmail.com>
2026-02-20 18:28:16 +0530

    image_upload_on_cloudinary

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,14 +1,20 @@
 import { v2 as cloudinary } from 'cloudinary'
 
-const uploadOnCloudinary = async (file) => {
-    cloudinary.config({ 
-  cloud_name: 'my_cloud_name', 
-  api_key: 'my_key', 
-  api_secret: 'my_secret'
-});
+const uploadOnCloudinary = async(file) => {
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET_KEY
+    });
     try {
-    }
-    catch (error) {
+        const result = await cloudinary.uploader.upload(file)
+        FileSystem.unlinkSync(file)
+        return result.secure_url;
+
+    } catch (error) {
+        FileSystem.unlinkSync(file)
+        console.error("Cloudinary upload failed:", error);
+        throw error;
     }
 }
 
